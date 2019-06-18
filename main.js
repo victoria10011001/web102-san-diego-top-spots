d147092d336da45cfba7934920d00242654b9a70
victoria <victoria10011001@hotmail.com>
2019-06-18 16:31:10 -0700

    project commit

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON("data.json", function(data) {
+        var items = [];
+        $.each(data, function(index, row) {
+            items.push("<tr>")
+            $.each(row, function(key, val) {
+                if(key === "location") {
+                    var link = 'https://www.google.com/maps?q=' + val.join(",")
+                    items.push('<td><a target="_blank" class="btn btn-primary" href="' + link + '">Open in Google Maps</a></td>')
+                } else {
+                    items.push("<td>" + val + "</td>") 
+                }
+            })
+            items.push("</tr>");
+        })
+        $("tbody").append(items.join(""));
+    })
 });
